39ce97d931425a9a76e806da5d7927e34f6b02e5
cbrooks-dev <connorbrooks04@yahoo.com>
2025-11-26 22:51:39 -0500

    Add initial UI

--- core/views.py
+++ core/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    context = {
+        "title": "Home | Track It",
+    }
+    return render(request, "index.html", context)
