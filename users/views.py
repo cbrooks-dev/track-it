d0761e621d0c72d7e559db2d086e01e30d5afab3
cbrooks-dev <connorbrooks04@yahoo.com>
2025-11-29 21:07:32 -0500

    Update auth

--- users/views.py
+++ users/views.py
@@ -1,41 +1,42 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, logout, login
-from django.contrib.auth.models import User
 from django.contrib import messages
+from forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def register_user(request):
     if request.method == "POST":
-        # TODO: validate input
-        user = User.objects.create_user(
-            request["username"],
-            request["email"],
-            request["password"],
-        )
-        login(request, user)
-        messages.success(request, "Successful registration!")
-        return redirect("index")
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Successful registration!")
+            return redirect("login")
     else:
-        context = {}  # TODO: fill in context 
-        return render(request, "register.html", context)
+        form = RegistrationForm()
+    context = {}  # TODO: fill in context (remember to add form) 
+    return render(request, "register.html", context)
 
 
 def login_user(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            # Redirect to a success page.
-            messages.success(request, "Successful login!")
-            return redirect("index")
+        form = AuthenticationForm(request)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Redirect to a success page.
+                messages.success(request, "Successful login!")
+                return redirect("index")
         else:
             # Return an 'invalid login' error message.
             messages.error(request, "Login failed. Please try again.")
     else:
-        context = {}  # TODO: fill in context
-        return render(request, "login.html", context)
+        form = AuthenticationForm()
+    context = {}  # TODO: fill in context (remember to add form)
+    return render(request, "login.html", context)
 
 
 def logout_user(request):
